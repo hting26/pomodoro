a543e3d1bc7b0da2de4b71834e9dab5172faff8c
hting <26hting@gmail.com>
2021-12-14 08:20:26 +0800

    1214

---
+++ src/plugins/bootstrap-vue.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(IconsPlugin)
+Vue.use(BootstrapVue)
